a7e14046c8cf616b8fb97bf6ed68cd18832663a3
Daniel Dao <danieldkdao@gmail.com>
2025-12-11 16:12:28 -0600

    created interview index page

---
+++ src/features/interviews/dbCache.ts
@@ -0,0 +1,31 @@
+import {
+  getGlobalTag,
+  getIdTag,
+  getJobInfoTag,
+  getUserTag,
+} from "@/lib/dataCache";
+import { revalidateTag } from "next/cache";
+
+export const getInterviewGlobalTag = () => {
+  return getGlobalTag("jobInfos");
+};
+
+export const getInterviewJobInfoTag = (jobInfoId: string) => {
+  return getJobInfoTag("jobInfos", jobInfoId);
+};
+
+export const getInterviewIdTag = (id: string) => {
+  return getIdTag("interviews", id);
+};
+
+export const revalidateJobInfosCache = ({
+  id,
+  jobInfoId,
+}: {
+  id: string;
+  jobInfoId: string;
+}) => {
+  revalidateTag(getInterviewGlobalTag(), "max");
+  revalidateTag(getInterviewJobInfoTag(jobInfoId), "max");
+  revalidateTag(getInterviewIdTag(id), "max");
+};
--- src/lib/dataCache.ts
+++ src/lib/dataCache.ts
@@ -8,6 +8,10 @@ export const getUserTag = (tag: CacheTag, userId: string) => {
   return `user:${userId}:${tag}` as const;
 };
 
+export const getJobInfoTag = (tag: CacheTag, jobInfoId: string) => {
+  return `jobInfo:${jobInfoId}:${tag}` as const;
+};
+
 export const getIdTag = (tag: CacheTag, id: string) => {
   return `id:${id}:${tag}` as const;
 }
